77ec7ca3f78733e43fb2b2ccc4bf3665e61ded20
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-11-28 15:19:52 -0700

    keep using spack-stack-1.5.1 on jet as 1.9.2 is not a full installation and misses some modules needed by RRFS_UTILS

--- modulefiles/build_jet_intel.lua
+++ modulefiles/build_jet_intel.lua
@@ -1,26 +1,27 @@
 help([[
-This module loads libraries for rrfs-workflow
+This module loads libraries for building the RRFS workflow on
+the NOAA RDHPC machine Jet using Intel-2022.1.2
 ]])
 
-whatis([===[Loads libraries for rrfs-workflow ]===])
-prepend_path("MODULEPATH","/mnt/lfs6/BMC/nrtrr/spack-stack/1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
+whatis([===[Loads libraries needed for building the RRFS workflow on Jet ]===])
 
-load("stack-oneapi/2024.2.1")
-load("stack-intel-oneapi-mpi/2021.13")
-load("cmake/3.27.9")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.5.1/envs/gsi-addon-rocky8/install/modulefiles//Core")
 
-load("parallelio/2.6.2")
+load("stack-intel/2021.5.0")
+load("stack-intel-oneapi-mpi/2021.5.1")
+load("parallelio/2.5.10")
 load("jasper/2.0.32")
 load("libpng/1.6.37")
-load("g2/3.5.1")
-load("g2tmpl/1.13.0")
+load("g2/3.4.5")
+--load("g2tmpl/1.10.2")
+setenv("g2tmpl_ROOT","/lfs5/BMC/wrfruc/mhu/rrfs/lib/g2tmpl/install")
 load("w3emc/2.10.0")
 load("w3nco/2.4.1")
-load("wgrib2/3.6.0")
+load("wgrib2/2.0.8")
 load("ncio/1.1.2")
-load("nco/5.2.4")
-load("bufr/12.1.0")
+load("nco/5.0.6")
+load("bufr/11.7.0")
 
-setenv("CMAKE_C_COMPILER", "mpiicc")
-setenv("CMAKE_CXX_COMPILER", "mpiicpc")
-setenv("CMAKE_Fortran_COMPILER", "mpiifort")
+setenv("CMAKE_C_COMPILER","mpiicc")
+setenv("CMAKE_CXX_COMPILER","mpiicpc")
+setenv("CMAKE_Fortran_COMPILER","mpiifort")
